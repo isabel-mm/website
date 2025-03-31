6a11c104eb9512658ebfa6492d963fb02570a033
Isabel <83420259+isabel-mm@users.noreply.github.com>
2025-03-31 13:07:10 +0200

    Update include.js

--- js/include.js
+++ js/include.js
@@ -12,18 +12,17 @@ function includeHTML() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  includeHTML();
+document.addEventListener("DOMContentLoaded", includeHTML);
 
-  // Espera a que los componentes estén cargados antes de añadir eventos
-  document.addEventListener("click", function (event) {
-    if (event.target.matches(".dropbtn")) {
-      document.getElementById("dropdownMenu").classList.toggle("show");
-    } else {
-      const dropdowns = document.getElementsByClassName("dropdown-content");
-      for (let i = 0; i < dropdowns.length; i++) {
-        dropdowns[i].classList.remove("show");
-      }
+function toggleDropdown() {
+  document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].classList.remove('show');
     }
-  });
-});
+  }
+};
